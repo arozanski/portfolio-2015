49f112c12e24332a6f9ae611fa7b2cb06af9163d
Armand Rozanski <armand@rozanski.pro>
2015-03-29 00:43:51 +0000

    home section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	'use strict';
 
-	$('#sidenav').localScroll(800);
+	$.localScroll(800);
 	
 	//.parallax(xPosition, speedFactor, outerHeight) options:
 	//xPosition - Horizontal position of the element
 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
 	$('#home').parallax('50%', 0.1);
-	$('#home').css({ 'height': window.innerHeight });
+	$('section').css({ 'height': window.innerHeight });
 	$('#about').parallax('50%', 0.1);
 	$('.bg').parallax('50%', 0.4);
 	$('#third').parallax('50%', 0.3);
