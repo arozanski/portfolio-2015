526f812c4c7ee1a0e6edb1647cb59c97a7f7489a
Armand Rozanski <armand@rozanski.pro>
2015-04-04 13:32:32 +0100

    other projects section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,6 +11,7 @@ var app = (function () {
 
 	var _bind = function() {
 		window.onresize = _adjust;  
+		window.onload = _adjust;
 		_inview($('#skills'));	
 		_inview($('#scene-v6'));	
 		_inview($('#scene-360'));	
@@ -35,9 +36,9 @@ var app = (function () {
 		//xPosition - Horizontal position of the element
 		//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 		//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-		$('#home').parallax('50%', 0.1);
+		$('#home').parallax('50%', 0.15);
 		$('#skills').parallax('50%', 0.3);
-		$('#other-projects').parallax('50%', 0.2);
+		//$('#other-projects').parallax('50%', 0.2);
 	};
 
 	var _adjust = function() {
